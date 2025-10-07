dc42c621bb79cd9c1428bee9083830fd0f7f791e
Dinis Cruz <dinis.cruz@owasp.org>
2025-10-07 23:17:47 +0100

    updated to osbot-fast-api version

--- mgraph_ai_service_cache_client/client_builder/Fast_API__Client__Builder.py
+++ mgraph_ai_service_cache_client/client_builder/Fast_API__Client__Builder.py
@@ -1,6 +1,7 @@
 from osbot_fast_api.client.Fast_API__Client__Generator  import Fast_API__Client__Generator
 from osbot_utils.type_safe.Type_Safe                    import Type_Safe
 from mgraph_ai_service_cache.fast_api.Service__Fast_API import Service__Fast_API
+from mgraph_ai_service_cache_client                     import client_contract
 
 
 class Fast_API__Client__Builder(Type_Safe):
@@ -10,7 +11,11 @@ class Fast_API__Client__Builder(Type_Safe):
         self.cache_service__fast_api = Service__Fast_API().setup()
         self.generator               = Fast_API__Client__Generator(fast_api=self.cache_service__fast_api)
 
-    def create_client_files(self, output_dir):
+    def create_client_files(self):
         with self.generator as _:
+            output_dir  = self.target_folder()
             saved_files = _.save_client_files(output_dir=output_dir)
             return sorted(saved_files)
+
+    def target_folder(self):
+        return client_contract.path
--- tests/unit/client_builder/test_Fast_API__Client__Builder.py
+++ tests/unit/client_builder/test_Fast_API__Client__Builder.py
@@ -1,5 +1,8 @@
 import tempfile
 from unittest                                                                import TestCase
+
+from osbot_utils.utils.Files import folder_name, parent_folder_name
+
 from mgraph_ai_service_cache_client.client_builder.Fast_API__Client__Builder import Fast_API__Client__Builder
 
 
@@ -11,22 +14,27 @@ class test_Fast_API__Client__Builder(TestCase):
 
 
     def test_create_files(self):
-        with tempfile.TemporaryDirectory() as temp_dir:
-            saved_files = self.client_builder.create_client_files(temp_dir)
-            assert saved_files == [ 'Service__Fast_API__Client.py'                              ,
-                                    'Service__Fast_API__Client__Config.py'                      ,
-                                    'Service__Fast_API__Client__Requests.py'                    ,
-                                    'admin_storage/Service__Fast_API__Client__Admin__Storage.py',
-                                    'auth/Service__Fast_API__Client__Set_Cookie.py'             ,
-                                    'data/Service__Fast_API__Client__Data.py'                   ,
-                                    'data/Service__Fast_API__Client__Data__Delete.py'           ,
-                                    'data/Service__Fast_API__Client__Data__Retrieve.py'         ,
-                                    'data_store/Service__Fast_API__Client__Data__Store.py'      ,
-                                    'delete/Service__Fast_API__Client__File__Delete.py'         ,
-                                    'exists/Service__Fast_API__Client__File__Exists.py'         ,
-                                    'info/Service__Fast_API__Client__Info.py'                   ,
-                                    'namespace/Service__Fast_API__Client__Namespace.py'         ,
-                                    'retrieve/Service__Fast_API__Client__File__Retrieve.py'     ,
-                                    'server/Service__Fast_API__Client__Server.py'               ,
-                                    'store/Service__Fast_API__Client__File__Store.py'           ,
-                                    'zip/Service__Fast_API__Client__Zip.py'                     ]
+        saved_files = self.client_builder.create_client_files()
+        assert saved_files == [ 'Service__Fast_API__Client.py'                              ,
+                                'Service__Fast_API__Client__Config.py'                      ,
+                                'Service__Fast_API__Client__Requests.py'                    ,
+                                'admin_storage/Service__Fast_API__Client__Admin__Storage.py',
+                                'auth/Service__Fast_API__Client__Set_Cookie.py'             ,
+                                'data/Service__Fast_API__Client__Data.py'                   ,
+                                'data/Service__Fast_API__Client__Data__Delete.py'           ,
+                                'data/Service__Fast_API__Client__Data__Retrieve.py'         ,
+                                'data_store/Service__Fast_API__Client__Data__Store.py'      ,
+                                'delete/Service__Fast_API__Client__File__Delete.py'         ,
+                                'exists/Service__Fast_API__Client__File__Exists.py'         ,
+                                'info/Service__Fast_API__Client__Info.py'                   ,
+                                'namespace/Service__Fast_API__Client__Namespace.py'         ,
+                                'retrieve/Service__Fast_API__Client__File__Retrieve.py'     ,
+                                'server/Service__Fast_API__Client__Server.py'               ,
+                                'store/Service__Fast_API__Client__File__Store.py'           ,
+                                'zip/Service__Fast_API__Client__Zip.py'                     ]
+
+    def test_target_folder(self):
+        with self.client_builder  as _:
+            target_folder = _.target_folder()
+            assert folder_name       (target_folder) == 'client_contract'
+            assert parent_folder_name(target_folder) == 'mgraph_ai_service_cache_client'
