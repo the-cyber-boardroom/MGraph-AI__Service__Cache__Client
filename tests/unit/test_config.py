a5281d4bb6213700f8b2682ba58b107b399f32a8
Dinis Cruz <dinis.cruz@owasp.org>
2025-10-07 19:20:13 +0100

    removed non needed files for this client

--- tests/unit/test_config.py
+++
@@ -1,8 +0,0 @@
-from unittest                          import TestCase
-from mgraph_ai_service_cache_client.config      import SERVICE_NAME
-
-
-class test_config(TestCase):
-
-    def test__config_vars(self):
-        assert SERVICE_NAME                   == 'mgraph_ai_service_cache_client'
