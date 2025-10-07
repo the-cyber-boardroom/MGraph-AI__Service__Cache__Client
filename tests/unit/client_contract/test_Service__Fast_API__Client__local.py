0ce12186fc5840a5fbda217ee5326097008f9a3a
Dinis Cruz <dinis.cruz@owasp.org>
2025-10-08 00:28:04 +0100

    added test_info to local server

--- tests/unit/client_contract/test_Service__Fast_API__Client__local.py
+++ tests/unit/client_contract/test_Service__Fast_API__Client__local.py
@@ -1,14 +1,13 @@
-from unittest import TestCase
-
-from osbot_fast_api.utils.Fast_API_Server import Fast_API_Server
-from osbot_fast_api_serverless.fast_api.Serverless__Fast_API__Config import Serverless__Fast_API__Config
-from osbot_utils.helpers.duration.decorators.capture_duration import capture_duration
-from osbot_utils.helpers.duration.decorators.print_duration import print_duration
-from osbot_utils.utils.Http import GET, GET_json
-
-from mgraph_ai_service_cache.fast_api.Service__Fast_API import Service__Fast_API
-from mgraph_ai_service_cache.utils.Version import version__mgraph_ai_service_cache
+from unittest                                                                 import TestCase
+from osbot_fast_api.utils.Fast_API_Server                                     import Fast_API_Server
+from osbot_fast_api_serverless.fast_api.Serverless__Fast_API__Config          import Serverless__Fast_API__Config
+from osbot_utils.helpers.duration.decorators.capture_duration                 import capture_duration
+from osbot_utils.testing.__ import __
+from osbot_utils.utils.Http                                                   import GET_json
+from mgraph_ai_service_cache.fast_api.Service__Fast_API                       import Service__Fast_API
+from mgraph_ai_service_cache.utils.Version                                    import version__mgraph_ai_service_cache
 from mgraph_ai_service_cache_client.client_contract.Service__Fast_API__Client import Service__Fast_API__Client
+from mgraph_ai_service_cache_client.client_contract.Service__Fast_API__Client__Config import Service__Fast_API__Client__Config
 
 
 class test_Service__Fast_API__Client__local(TestCase):
@@ -20,6 +19,10 @@ class test_Service__Fast_API__Client__local(TestCase):
             cls.cache_service__fast_api = Service__Fast_API(config=cls.serverless_config).setup()
             cls.fast_api_server         = Fast_API_Server(app=cls.cache_service__fast_api.app())
             cls.server_url              = cls.fast_api_server.url()
+
+            cls.server_config           = Service__Fast_API__Client__Config(base_url=cls.server_url, verify_ssl=False)
+            cls.fast_api_client         = Service__Fast_API__Client        (config=cls.server_config)
+
             cls.fast_api_server.start()
 
         assert duration.seconds < 0.5               # server setup and start should not take more than 0.5 (locally takes about 0.25)
@@ -35,3 +38,20 @@ class test_Service__Fast_API__Client__local(TestCase):
                                                                'name'       : 'mgraph_ai_service_cache'       ,
                                                                'status'     : 'operational'                   ,
                                                                'version'    : version__mgraph_ai_service_cache}
+    def test__init__(self):
+        with self.fast_api_client as _:
+            assert _.config.obj() == __(base_url       = self.server_url    ,
+                                       api_key         = None               ,
+                                       api_key_header  = 'X-API-Key'        ,
+                                       timeout         = 30                 ,
+                                       verify_ssl      = False              ,
+                                       service_name    ='Service__Fast_API' ,
+                                       service_version = 'v0.5.67'          )
+
+    def test_info(self):
+        with self.fast_api_client.info() as _:
+            assert _.health() == { 'status'     : 'ok'                             }
+            assert _.status() == { 'environment': 'local'                          ,
+                                   'name'       : 'mgraph_ai_service_cache'        ,
+                                   'status'     : 'operational'                    ,
+                                   'version'    : version__mgraph_ai_service_cache }
